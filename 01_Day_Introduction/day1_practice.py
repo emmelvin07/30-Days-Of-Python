d3a65dece941ec43ab11e128c95799af5210bacd
Emmelvin John <emmelvinjohn@gmail.com>
2026-01-21 11:36:27 +0800

    Complete Day 1 exercises

---
+++ 01_Day_Introduction/day1_practice.py
@@ -0,0 +1,18 @@
+# Day 1 - 30 Days of Python
+
+print(3 + 4)      # addition
+print(3 - 4)      # subtraction
+print(3 * 4)      # multiplication
+print(3 % 4)      # modulus
+print(3 / 4)      # division
+print(3 ** 4)     # exponential
+print(3 // 4)     # floor division
+
+print(type(10))                             # int
+print(type(9.8))                            # float
+print(type(3.14))                           # float
+print(type(4 - 4j))                         # complex
+print(type(['Emmelvin', 'John', 'Encio']))  # list
+print(type("Kenma"))                        # string
+print(type("Kozume"))                       # string
+print(type("Philippines"))                  # string
