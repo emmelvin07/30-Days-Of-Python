f002534df331e36598197ce2653af9f4de617ef5
Emmelvin John <emmelvinjohn@gmail.com>
2026-01-22 20:59:54 +0800

    da2

--- 02_Day_Variables_builtin_functions/variables.py
+++ 02_Day_Variables_builtin_functions/variables.py
@@ -1,25 +1,25 @@
 
 # Variables in Python
 
-first_name = 'Asabeneh'
-last_name = 'Yetayeh'
-country = 'Finland'
-city = 'Helsinki'
-age = 250
-is_married = True
+first_name = 'Emmelvin'
+last_name = 'Encio'
+country = 'Philippines'
+city = 'San Pedro'
+age = 22
+is_married = False
 skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
 person_info = {
-    'firstname': 'Asabeneh',
-    'lastname': 'Yetayeh',
-    'country': 'Finland',
-    'city': 'Helsinki'
+    'firstname': 'Emmelvin',
+    'lastname': 'Encio',
+    'country': 'Philippines',
+    'city': 'San Pedro'
 }
 
 # Printing the values stored in the variables
 
 print('First name:', first_name)
 print('First name length:', len(first_name))
-print('Last name: ', last_name)
+print('Last name: ', last_name)     
 print('Last name length: ', len(last_name))
 print('Country: ', country)
 print('City: ', city)
@@ -30,7 +30,7 @@ print('Person information: ', person_info)
 
 # Declaring multiple variables in one line
 
-first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Helsink', 250, True
+first_name, last_name, country, age, is_married = 'Emmelvin', 'Encio', 'Philippines', 22, False
 
 print(first_name, last_name, country, age, is_married)
 print('First name:', first_name)
